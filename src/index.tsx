74138c32db36b17d61346c62ac48c3f9c2c2518e
daaaj <daaa_j@naver.com>
2023-09-12 14:08:39 +0900

    Initial init

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider } from 'react-router-dom';
+import { Router } from './router/Router';
+import { ThemeProvider } from 'styled-components';
+import { Theme } from './style/Theme';
+import { GlobalStyle } from './style/GlobalStyel';
+import { RecoilRoot } from 'recoil';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RecoilRoot>
+        <ThemeProvider theme={Theme}>
+            <GlobalStyle />
+            <RouterProvider router={Router} />
+        </ThemeProvider>
+    </RecoilRoot>
 );
 
 // If you want to start measuring performance in your app, pass a function
