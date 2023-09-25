21924dbdc40d7d76dcfb2f0764d8acb895cd347e
daaaj <daaa_j@naver.com>
2023-09-25 22:01:17 +0900

    refactor : 포트폴리오 수정

--- src/components/detail/DetailPortfolio.tsx
+++ src/components/detail/DetailPortfolio.tsx
@@ -51,48 +51,47 @@ export default function DetailPortfolio() {
               <TableTitle>담당 역할</TableTitle>
             </div>
             <ul>
-              <li>React + Typescript를 사용한 정적 웹 페이지 개발</li>
-              <li>다크모드 구현</li>
               <li>
-                <LinkText to="https://daaaj.tistory.com/54" target="_blank">
-                  유저 피드백 반영하여 서비스 성능 개선(70% 반영 완료) ↗
+                <LinkText to="https://daaaj.tistory.com/57" target="_blank">
+                  S3 배포시 github-action 이용하여 CI/CD 구축 ↗
                 </LinkText>
               </li>
+              <li>google analytics 적용</li>
+              <li>다크모드 구현</li>
+              <li>웹 반응형</li>
             </ul>
           </TableBox>
           <TableBox>
             <div>
               <TableTitle>홈페이지</TableTitle>
             </div>
-            <LinkSiteText to="https://www.daydei.life" target="_blank">
-              www.daydei.life
+            <LinkSiteText to="http://portfolio-jeong.s3-website.ap-northeast-2.amazonaws.com" target="_blank">
+              @portfolio-aws
             </LinkSiteText>
           </TableBox>
           <TableBox>
             <div>
               <TableTitle>깃허브</TableTitle>
             </div>
-            <LinkSiteText to="https://github.com/DAYdeiProject/DAYdei_FE" target="_blank">
-              @github/daydei
-            </LinkSiteText>
-          </TableBox>
-          <TableBox>
-            <div>
-              <TableTitle>노션</TableTitle>
-            </div>
-            <LinkSiteText to="https://www.notion.so/A-4-SA-daydei-ae22a288543d455c810012fd6ac9b3a0" target="_blank">
-              @notion/daydei
+            <LinkSiteText to="https://github.com/daaaj/mypage" target="_blank">
+              @github/mypage
             </LinkSiteText>
           </TableBox>
         </DetailiTableBox>
         <DetailDescBox>
           <DescBox>
-            <h4></h4>
-            <p></p>
-          </DescBox>
-          <DescBox>
-            <h4></h4>
-            <p></p>
+            <h4>회고</h4>
+            <p>
+              포트폴리오 웹 사이트를 개발하며, 타입스크립트와 리코일을 활용한 경험을 통해 기술적으로 성장할 수 있는 기회가 되었습니다. 프로젝트의
+              초기부터 고려한 주요 기능 중 하나는 다크모드 기능이었습니다. 디폴트로는 다크모드 설정이 되어 있으며, 사용자가 원하는 화면 모드를 선택할
+              수 있도록 하는 UX적 고려를 통해 적용하게 되었습니다.
+              <br /> 또한, 코드의 재사용성을 극대화하기 위해 atomic한 컴포넌트 개발에 주력했습니다. 이를 통해 작은 단위의 컴포넌트로 나누고
+              재사용함으로써 코드의 확장성을 높였습니다. 더불어, 공통 비즈니스 로직은 커스텀 훅으로 만들어 최대한 중복 코드를 최소화하여 개발
+              하였습니다.
+              <br />
+              이러한 경험을 통해, 타입스크립트와 리코일을 활용한 웹 개발 능력을 향상시키게 되었고, UX 측면에서도 사용자 편의성을 고려하는 방법을 배울
+              수 있었습니다.
+            </p>
           </DescBox>
         </DetailDescBox>
       </DetailDescWrapper>
